09ebbb8ac3973569b407e0b2478afe3d4ffe1d3b
PrajwalSomashekara13 <58339536+prajwalsoma13@users.noreply.github.com>
2021-09-07 14:01:20 +0530

    Add files via upload

---
+++ sorting/InsertionSort.py
@@ -0,0 +1,17 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j, j - 1, array)
+            j -= 1
+
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+
+
+x = [8, 5, 2, 9, 5, 6, 3]
+y = insertionSort(x)
+print(y)
