92b60c5b4eefa8f1d519dabb3c5bd1423c65d0c9
PrajwalSomashekara13 <58339536+prajwalsoma13@users.noreply.github.com>
2021-09-07 14:01:40 +0530

    Add files via upload

---
+++ famousAlgorithms/kadaneAlgo.py
@@ -0,0 +1,12 @@
+def kadaneAlgorithm(array):
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+    for num in array[1:]:
+        maxEndingHere = max(num, maxEndingHere + num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+
+    return print(maxSoFar)
+
+
+a = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+kadaneAlgorithm(a)
